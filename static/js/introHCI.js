feeb7b8a1379cc697e4f8589d94148d0b0618ce6
Michelle Nguyen <mnn027@ucsd.edu>
2018-01-18 02:03:41 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,24 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
 /*
  * Function that is called when the document is ready.
  */
+
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	//Click listener
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
 }
